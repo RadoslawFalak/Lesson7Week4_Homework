e161207bb53b8480054f54fb854b3116b6b34045
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-07 08:17:45 +0200

    add class Exercise3 with method isPositiveOrNot

--- Lesson7Week4/Exercise3.cs
+++ Lesson7Week4/Exercise3.cs
@@ -10,7 +10,23 @@ namespace Lesson7Week4
     {
         public static void isPositiveOrNot() 
         {
-            
+            int userNumber;
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out userNumber);
+
+            if ( userNumber != 0 && userNumber > 0) 
+            {
+                Console.WriteLine(userNumber + " jest liczba dodatnia");
+            }
+            else if (userNumber != 0 && userNumber < 0)
+            {
+                Console.WriteLine(userNumber + " jest liczba ujemna");
+            }
+            else
+            {
+                Console.WriteLine("Podaj liczbe rozna od zera !");
+            }
         }
        
     }
