e28777685952c6133d39c94bee7bb270bea85ec3
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-07 07:50:47 +0200

    add class Exercise1 with method isEqual

---
+++ Lesson7Week4/Exercise1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise1
+    {
+        public static void isEqual()
+        {
+            int a, b;
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out a);
+            input = Console.ReadLine();
+            int.TryParse(input, out b);
+
+            if ( a == b )
+            {
+                Console.WriteLine(a + " i " + b + " sa rowne");
+            }
+            else
+            {
+                Console.WriteLine(a + " i " + b + " nie sa rowne");
+            }
+
+        }
+            
+    }
+}
