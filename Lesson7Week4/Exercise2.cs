ab56947e4b3c2744a7d71fb7aba04b3b8d021519
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-07 08:10:47 +0200

    add class Exercise2 with method isEvenOrNot

---
+++ Lesson7Week4/Exercise2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise2
+    {
+        public static void isEvenOrNot()
+        {
+            int userNumber;
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out userNumber);
+
+            if ( userNumber != 0 && userNumber %2 == 0 )
+            {
+                Console.WriteLine(userNumber + " jest liczba parzysta");
+            }
+            else if ( userNumber == 0 )
+            {
+                Console.WriteLine("Podaj liczbe rozna od zera");
+            }
+            else 
+            {
+                Console.WriteLine(userNumber + " jest liczba nieparzysta");
+            }
+
+        }
+    }
+}
---
+++ Lesson7Week4/Exercise3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise3
+    {
+        public static void isPositiveOrNot() 
+        {
+            
+        }
+       
+    }
+}
