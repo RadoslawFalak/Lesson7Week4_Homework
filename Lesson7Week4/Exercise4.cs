80cf9bdc87e54dc60676556e98e6b268219197df
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-07 08:37:28 +0200

    add class Exercise4 with method IsItLeapYear

---
+++ Lesson7Week4/Exercise4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise4
+    {
+        public static void IsItLeapYear()
+        {
+            int year;
+            string input = Console.ReadLine();
+            int.TryParse(input, out year);
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine(year + " jest rokiem przestepnym");
+            }
+            else
+            {
+                Console.WriteLine(year + " nie jest rokiem przestepnym");
+            }
+        }
+    }
+}
