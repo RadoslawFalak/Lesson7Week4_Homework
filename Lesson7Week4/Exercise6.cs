06b566d6ed15d52b50de2eca9fdff4576b1d68ee
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-12 22:47:54 +0200

    add class Exercise6 with method howHight

---
+++ Lesson7Week4/Exercise6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise6
+    {
+        public static void howHight()
+        {
+            int userHight;
+            string input = Console.ReadLine();
+            int.TryParse(input, out userHight); 
+
+            if (userHight  < 120 && userHight > 50) 
+            {
+                Console.WriteLine("Jestes dzieckiem");
+            }
+            else if (userHight > 120 && userHight <= 140)
+            {
+                Console.WriteLine("Jestes krasnoludem");
+            }
+            else if (userHight > 140 && userHight <= 180)
+            {
+                Console.WriteLine("Jestes czlowiekiem");
+            }
+            else if (userHight > 180 && userHight <= 240)
+            {
+                Console.WriteLine("Jesteś wielkoludem");
+            }
+            else if (userHight > 240 && userHight <= 350)
+            {
+                Console.WriteLine("Jestes trolem");
+            }
+            else
+            {
+                Console.WriteLine("Bledne dane");
+            }
+        }
+    }
+}
