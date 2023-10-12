9c808bda0ba1dc3f01f2726f23d4473608f0cd15
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-12 23:25:32 +0200

    add Exercises7 with method greatestValue

---
+++ Lesson7Week4/Exercise7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise7
+    {
+        public static void greatestValue()
+        {
+            int a, b, c;
+
+            string input = Console.ReadLine();
+            bool intCheck= int.TryParse(input, out a);
+            input = Console.ReadLine();
+            intCheck = int.TryParse(input, out b);
+            input = Console.ReadLine();
+            intCheck = int.TryParse(input, out c);
+
+
+            if ( intCheck != true)
+            {
+                Console.WriteLine("Musisz podac co najmniej jedna liczbe !");
+            }
+            else if ( a > b && a > c ) 
+            {
+                Console.WriteLine(a + " jest najwieksza z podanych");
+            }
+            else if ( b > c )
+            {
+                Console.WriteLine(b + " jest najwieksza z podanych");
+            }
+            else
+            {
+                Console.WriteLine(c + " jest najwieksza z podanych");
+            }
+        }
+    }
+}
--- Lesson7Week4/Program.cs
+++ Lesson7Week4/Program.cs
@@ -1,7 +1,9 @@
-﻿internal class Program
+﻿using Lesson7Week4;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-       
+        Exercise7.greatestValue();
     }
 }
