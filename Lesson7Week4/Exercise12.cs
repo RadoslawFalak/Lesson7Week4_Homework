92ef1f0308a161d743ba95d77108427729f98874
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-14 13:12:20 +0200

    add Exercise12 with methods DaysOfTheWeek , NameDayOfTheWeek

---
+++ Lesson7Week4/Exercise12.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise12
+    {
+        enum DaysOfTheWeek
+        {
+            Poniedzialek = 1,
+            Wtorek,
+            Sroda,
+            Czwartek,
+            Piatek,
+            Sobota,
+            Niedziela
+        }
+        public static void NameDayOfTheWeek()
+        {
+            int dayNumber;
+            string input = Console.ReadLine();
+            int.TryParse(input, out dayNumber);
+
+            if (dayNumber >= 1 && dayNumber <= 7)
+            {
+                Console.WriteLine((DaysOfTheWeek)dayNumber);
+            }
+            else
+            {
+                Console.WriteLine("Podaj cyfre z zakresu 1 - 7");
+            }
+        }
+    }
+}
--- Lesson7Week4/Program.cs
+++ Lesson7Week4/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Exercise11.GradesNumberToText();
+        Exercise12.NameDayOfTheWeek();
     }
 }
