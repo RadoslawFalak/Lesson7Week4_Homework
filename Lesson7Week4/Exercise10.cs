3e7aaf8e32d042692aea5321918fd78edad90d65
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-14 12:28:55 +0200

    add Exercise11 witch method GradesNumberToText

---
+++ Lesson7Week4/Exercise10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal class Exercise10
+    {
+        public static void Triangle() 
+        {
+            int a, b, c;
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out a);
+            input = Console.ReadLine();
+            int.TryParse(input, out b);
+            input = Console.ReadLine();
+            int.TryParse(input, out c);
+
+            if ( a + b > c )
+            {
+                Console.WriteLine("Można zbudować trójkąt");
+            }
+            else
+            {
+                Console.WriteLine("Nie można zbudować trójkąta");
+            }
+
+        }
+    }
+}
---
+++ Lesson7Week4/Exercise11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Week4
+{
+    internal partial class Exercise11
+    {
+        public static void GradesNumberToText() 
+        {
+            int grade;
+            string input = Console.ReadLine();
+            int.TryParse(input, out grade);
+
+            switch(grade)
+            {
+                case 1:
+                    Console.WriteLine("Niedostatateczny");
+                    break;
+                case 2:
+                    Console.WriteLine("Dopuszczajacy");
+                    break;
+                case 3:
+                    Console.WriteLine("Dostateczny");
+                    break;
+                case 4:
+                    Console.WriteLine("Dobry");
+                    break;
+                case 5:
+                    Console.WriteLine("Bardzo dobry");
+                    break;
+                case 6:
+                    Console.WriteLine("Celujacy");
+                    break;
+                default:
+                    Console.WriteLine("Podaj cyfre z zakresu 1 - 6");
+                    break;
+            }
+        }
+    }
+}
